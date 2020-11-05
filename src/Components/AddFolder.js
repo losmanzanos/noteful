f93c6095d989d3ea26fa1edb5ad9416bcf112b8f
Chad Moravec <chad@pop-os.localdomain>
2020-11-05 09:04:04 -0700

    addressed accessibility concerns

--- src/Components/AddFolder.js
+++ src/Components/AddFolder.js
@@ -1,60 +1,69 @@
-import React from 'react';
-import './AddFolder.css';
-import NoteContext from './context';
+import React from "react";
+import "./AddFolder.css";
+import NoteContext from "./context";
 
 class NewFolder extends React.Component {
-    static contextType = NoteContext;
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            error: null,
-        }
-        this.folderName = React.createRef();
-    }
+  static contextType = NoteContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      error: null,
+    };
+    this.folderName = React.createRef();
+  }
 
-    updateName(folderName) {
-        this.setState({ name: folderName });
-    }
+  updateName(folderName) {
+    this.setState({ name: folderName });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault();
 
-        fetch('http://localhost:9090/folders', {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(this.state)
-        })
-            .then((result) => result.json())
-            .then((info) => { this.context.getData() })
-            .catch(error => {
-                this.setState({
-                    error: error.message,
-                })
-            })
-    }
+    fetch("http://localhost:9090/folders", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((result) => result.json())
+      .then((info) => {
+        this.context.getData();
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message,
+        });
+      });
+  }
 
-    render() {
-        return (
-            <form className='addfolder' onSubmit={e => this.handleSubmit(e)} >
-                <div className='form-group'>
-                    <label htmlFor="folder">Folder Name:</label>
-                    <input type="text" className="foldername"
-                        name="folder" id="folder"
-                        ref={this.state.name}
-                        placeholder='Name'
-                        // defaultValue='Name'
-                        required
-                        onChange={e => this.updateName(e.target.value)} />
-                </div>
+  render() {
+    return (
+      <form className="addfolder" onSubmit={(e) => this.handleSubmit(e)}>
+        <div className="form-group">
+          <label htmlFor="folder">Folder Name:</label>
+          <input
+            type="text"
+            className="foldername"
+            name="folder"
+            id="folder"
+            ref={this.state.name}
+            placeholder="Name"
+            required
+            aria-label="Name of folder"
+            aria-required="true"
+            onChange={(e) => this.updateName(e.target.value)}
+          />
+        </div>
 
-                {this.state.error}
-                <button type='submit' className='foldersubmit'>Submit</button>
-            </form>
-        );
-    }
+        {this.state.error}
+        <button type="submit" className="foldersubmit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
 export default NewFolder;
--- src/Components/AddNote.js
+++ src/Components/AddNote.js
@@ -1,87 +1,100 @@
-import React from 'react';
-import './AddNote.css';
-import NoteContext from './context';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./AddNote.css";
+import NoteContext from "./context";
+import PropTypes from "prop-types";
 
 class NewNote extends React.Component {
-    state = {
-        error: null,
-    }
-    static contextType = NoteContext;
-    constructor(props) {
-        super(props);
-        this.noteName = React.createRef();
-        this.noteContent = React.createRef();
-        this.folderId = React.createRef();
-    }
+  state = {
+    error: null,
+  };
+  static contextType = NoteContext;
+  constructor(props) {
+    super(props);
+    this.noteName = React.createRef();
+    this.noteContent = React.createRef();
+    this.folderId = React.createRef();
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        const name = this.noteName.current.value;
-        const content = this.noteContent.current.value;
-        const modified = new Date();
-        const folderId = this.folderId.current.value;
-        const note = { name, content, folderId, modified };
+  handleSubmit(event) {
+    event.preventDefault();
+    const name = this.noteName.current.value;
+    const content = this.noteContent.current.value;
+    const modified = new Date();
+    const folderId = this.folderId.current.value;
+    const note = { name, content, folderId, modified };
 
-        fetch('http://localhost:9090/notes', {
-            method: "POST",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(note)
-        })
-            .then((result) => result.json())
-            .then((info) => {
-                this.context.getData()
-                this.props.history.push('/')
-            })
-            .catch(error => {
-                this.setState({
-                    error: error.message,
-                })
-            })
+    fetch("http://localhost:9090/notes", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(note),
+    })
+      .then((result) => result.json())
+      .then((info) => {
+        this.context.getData();
+        this.props.history.push("/");
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message,
+        });
+      });
+  }
 
-    }
+  render() {
+    return (
+      <form className="newnote" onSubmit={(e) => this.handleSubmit(e)}>
+        <div className="form-group">
+          <label htmlFor="note">Note Name:</label>
+          <input
+            type="text"
+            className="notename"
+            name="note"
+            id="note"
+            ref={this.noteName}
+            defaultValue="Name"
+            aria-label="Name of note"
+            aria-required="true"
+          />
+        </div>
 
-    render() {
-        return (
-            <form className='newnote' onSubmit={e => this.handleSubmit(e)}>
-                <div className='form-group'>
-                    <label htmlFor="note">Note Name:</label>
-                    <input type="text" className="notename"
-                        name="note" id="note"
-                        ref={this.noteName}
-                        defaultValue='Name' />
-                </div>
+        <div className="form-group">
+          <label htmlFor="notecontent">Content:</label>
+          <textarea
+            type="text"
+            className="notecontent"
+            name="notecontent"
+            id="notecontent"
+            ref={this.noteContent}
+            defaultValue="Blah blah blah..."
+            aria-label="Note content"
+            aria-required="true"
+          />
+        </div>
 
-                <div className='form-group'>
-                    <label htmlFor="notecontent">Content:</label>
-                    <textarea type="text" className="notecontent"
-                        name="notecontent" id="notecontent"
-                        ref={this.noteContent}
-                        defaultValue='Blah blah blah...' />
-                </div>
+        <div className="form-group">
+          <label htmlFor="notefolder">Folder:</label>
+          <select id="notefolder" name="notefolder" ref={this.folderId}>
+            {this.context.folders.map((folder) => (
+              <option key={folder.id} value={folder.id}>
+                {folder.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-                <div className='form-group'>
-                    <label htmlFor="notefolder">Folder:</label>
-                    <select id="notefolder" name="notefolder"
-                        ref={this.folderId}>
-                        {this.context.folders.map(folder =>
-                            (<option key={folder.id} value={folder.id}>{folder.name}</option>))}
-
-                    </select>
-                </div>
-
-                {this.state.error}
-                <button type='submit' className='notesubmit'>Submit</button>
-            </form>
-        );
-    }
+        {this.state.error}
+        <button type="submit" className="notesubmit">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
 NewNote.propTypes = {
-    history: PropTypes.any,
-}
-
+  history: PropTypes.any,
+};
 
 export default NewNote;
